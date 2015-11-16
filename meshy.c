5618f45140c43542a90b8b651e286ce953767f5b
Christina Johnson <christina.ann.johnson@gmail.com>
2015-11-15 22:27:41 -0800

    initialized polymec, created two meshes

--- meshy.c
+++ meshy.c
@@ -1,6 +1,6 @@
 #include <polymec/polymec.h>
 
-int main(int argc, char* argv[])
+void add_simple_3x3x3_mesh(silo_file_t* silo)
 {
   // Let's make the simplest damn mesh possible.
   int nx = 3, ny = 3, nz = 3;
@@ -9,13 +9,41 @@ int main(int argc, char* argv[])
                  .z1 = 0.0, .z2 = 1.0};
   mesh_t* simple = create_uniform_mesh(MPI_COMM_WORLD, nx, ny, nz, &bbox);
 
-  // Now create a SILO file for visualization, and write the mesh to it.
-  silo_file_t* silo = silo_file_new(MPI_COMM_WORLD, "meshy", ".", 1, 0, 0, 0.0);
+  // Add the mesh to the file.
   silo_file_write_mesh(silo, "simple", simple);
-  silo_file_close(silo);
 
   // Clean up.
   mesh_free(simple);
+}
+
+void add_cubed_c_mesh(silo_file_t* silo)
+{
+  mesh_t* cubed_c = create_cubed_cylinder_mesh(MPI_COMM_WORLD,
+                                   5, 5, 1.0, 1.0, 0.5, false, 
+                                   "R", "bottom", "top");
+
+
+  // Add the mesh to the file.
+  silo_file_write_mesh(silo, "cubed_c", cubed_c);
+
+  // Clean up.
+  mesh_free(cubed_c);
+}
+
+int main(int argc, char* argv[])
+{
+  // Initialize polymec.
+  polymec_init(argc, argv);
+
+  // Now create a SILO file for visualization.
+  silo_file_t* silo = silo_file_new(MPI_COMM_WORLD, "meshy", ".", 1, 0, 0, 0.0);
+
+  // Add meshes.
+  add_simple_3x3x3_mesh(silo);
+  add_cubed_c_mesh(silo);
+
+  // Write the SILO file.
+  silo_file_close(silo);
 
   return 0;
 }
